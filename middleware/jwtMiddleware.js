bc97a58be1913f4cd1a8e9fe8eb096e8f75f5f37
imsaikatsen <sensaikatcse@gmail.com>
2023-04-18 13:47:45 +0600

    initial commit

---
+++ middleware/jwtMiddleware.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const jwtMiddleware = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: 'Authentication failed' });
+      }
+      req.userId = decodedToken.userId;
+      next();
+    });
+  };
+  
+module.exports = jwtMiddleware;
